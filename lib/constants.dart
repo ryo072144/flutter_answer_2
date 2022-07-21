d624004b629ed2cf0348279b77cb76b382a45ab8
ryo072144 <84168405+ryo072144@users.noreply.github.com>
2022-07-21 10:26:59 +0900

    Update and rename widget.dart to constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+TextStyle titleStyle(){
+
+  //問５ titleTextはTextStyle型の変数である。このようにすれば毎回文字のスタイルを設定しなくても良い。
+  const TextStyle titleStyle = TextStyle(color: Color(0xffeeeeee), fontSize: 20 ,fontWeight: FontWeight.bold);
+}
